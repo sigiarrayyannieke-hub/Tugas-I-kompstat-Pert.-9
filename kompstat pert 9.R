1b8b8aecdb69193b559f19eb23352f8b803bd393
Arra <sigiarrayyannieke@gmail.com>
2026-05-11 11:51:51 +0700

    nambahin boxplot

--- kompstat pert 9.R
+++ kompstat pert 9.R
@@ -4,3 +4,8 @@ sd(mtcars$mpg)
 max(mtcars$mpg)
 min(mtcars$mpg)
 quantile(mtcars$mpg, 0,5)
+boxplot(mtcars$mpg,
+        main = "nyoba branch baru",
+        ylab = "gatau",
+        xlab = "iya",
+        col = "pink")
